ea3ffc410b314bb2bb48e3fde725677982ce1194
Eric <yuanshuaiding@163.com>
2018-03-09 18:05:34 +0800

    完善示例代码

--- app/src/main/java/com/eric/expandabletextview/RecycleActivity.java
+++ app/src/main/java/com/eric/expandabletextview/RecycleActivity.java
@@ -5,16 +5,23 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.eric.android.view.ExpandableTextView;
 import com.eric.android.view.ExpandableTextView4List;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecycleActivity extends AppCompatActivity {
-
+    private final int NULL = 0;
+    private final int OPEN = 1;
+    private final int CLOSE = 2;
+    private final int SMALL = 3;
+    private final int MAX_LINE = 3;
+    private SparseIntArray maps = new SparseIntArray();
     String txt = "从明天起做个幸福的人 喂马劈柴周游世界  从明天起关心粮食和蔬菜 我有一所房子  面朝大海春暖花开 从明天起和每一个亲人通信 告诉他们我的幸福 那幸福的闪电告诉我的 我将告诉每一个人 给每一条河每一座山取个温暖的名字 陌生人我也为你祝福 愿你有一个灿烂前程 愿你有情人终成眷属 愿你在尘世获得幸福 我只愿面朝大海春暖花开[[[[[[[[[[[[从明天起做个幸福的人 喂马劈柴周游世界  从明天起关心粮食和蔬菜 我有一所房子  面朝大海春暖花开 从明天起和每一个亲人通信 告诉他们我的幸福 那幸福的闪电告诉我的 我将告诉每一个人 给每一条河每一座山取个温暖的名字 陌生人我也为你祝福 愿你有一个灿烂前程 愿你有情人终成眷属 愿你在尘世获得幸福 我只愿面朝大海春暖花开";
 
     @Override
@@ -53,8 +60,24 @@ public class RecycleActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull VH holder, int position) {
-            holder.tv.setText(datas.get(position), position%10==0);
+        public void onBindViewHolder(@NonNull final VH holder, int position) {
+            final int status = maps.get(holder.getAdapterPosition(), NULL);
+            switch (status) {
+                case NULL:
+                case SMALL:
+                case CLOSE:
+                    holder.tv.setText(datas.get(position), false);
+                    break;
+                case OPEN:
+                    holder.tv.setText(datas.get(position), true);
+                    break;
+            }
+            holder.tv.setToggleListener(new ExpandableTextView.OnToggleListener() {
+                @Override
+                public void onToggle(boolean expanded) {
+                    maps.put(holder.getAdapterPosition(), expanded ? OPEN : CLOSE);
+                }
+            });
         }
 
         @Override
