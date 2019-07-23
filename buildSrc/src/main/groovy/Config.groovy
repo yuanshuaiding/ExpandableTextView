51a0ca7f5342fc06846ef69b03ea6acd049b3c19
Eric <yuanshuaiding@163.com>
2019-07-23 15:13:38 +0800

    修改依赖版本控制方式为groovy

---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,8 @@
+class Config {
+    static appid = 'com.eric.expandabletextview'//应用包名
+    static compileSdkVersion = 28
+    static buildToolsVersion = '28.0.3'
+    static minSdkVersion = 15
+    static targetSdkVersion = 28
+    static supportVersion = '28.0.0'
+}
